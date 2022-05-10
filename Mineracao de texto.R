1a14ad858ec80dffad3a24c2ad2795a97e911e8e
cibellelepore <cibellerodrigues@hotmail.com>
2022-05-10 19:29:38 -0300

    atualizando

--- Mineracao de texto.R
+++ Mineracao de texto.R
@@ -20,5 +20,22 @@ comentario <- Tabela_de_Comentarios %>% select(Comentário)
 
 texto <- comentario [-c(2:4,6,19:29),]
 
+# Transformando em tiblle 
+
+texto_df <- tibble(line = 1:14, text = texto)
+
+## Base Teste - para testar 
+teste_primeiras_linhas <- texto_df [1:5,]
+
+
+
+
+
+# transformando em tokens (quebrando em palavras)
+
+
+
 # Excluindo os números que aparecem nos comentários
+
+
 
